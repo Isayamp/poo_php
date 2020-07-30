9e6330fb52288903e420d058d1b4bbe2d96b9bf5
Isaya M Pombo <isayamlelapombo@gmail.com>
2020-07-30 10:59:23 +0200

    property of Administrateur added

--- utilisateur/Administrateur.php
+++ utilisateur/Administrateur.php
@@ -2,6 +2,15 @@
 
     namespace Utilisateur\Administrateur\Administrteur;
     use Utilisateur\Utilisateur\Utilisateur;
+    // require_once 'Utilisateur/Utilisateur.php';
+
+    class Administrateur extends Utilisateur
+    {
+        // Properties
+        private $code_secret;
+
+    }
+    
 
 
 ?>
