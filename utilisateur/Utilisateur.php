b7a34866fc0a43cd2bd1ca5a613eea8b3c3d9f89
Isaya M Pombo <isayamlelapombo@gmail.com>
2020-07-30 10:16:28 +0200

    Getters & Setters functions created

--- utilisateur/Utilisateur.php
+++ utilisateur/Utilisateur.php
@@ -11,12 +11,95 @@
         private $mail;
         private $password;
 
+        // Constructor
         public function __construct($nom, $prenom, $mail, $password)
         {
             $this->nom = $nom;
             $this->prenom = $prenom;
             $this->mail = $mail;
             $this->password = $password;
+        }
+
+        // Getters & Setters functions
+
+        /**
+         * Get the value of nom
+         */ 
+        public function getNom()
+        {
+                return $this->nom;
+        }
+
+        /**
+         * Set the value of nom
+         *
+         * @return  self
+         */ 
+        public function setNom($nom)
+        {
+                $this->nom = $nom;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of prenom
+         */ 
+        public function getPrenom()
+        {
+                return $this->prenom;
+        }
+
+        /**
+         * Set the value of prenom
+         *
+         * @return  self
+         */ 
+        public function setPrenom($prenom)
+        {
+                $this->prenom = $prenom;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of mail
+         */ 
+        public function getMail()
+        {
+                return $this->mail;
+        }
+
+        /**
+         * Set the value of mail
+         *
+         * @return  self
+         */ 
+        public function setMail($mail)
+        {
+                $this->mail = $mail;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of password
+         */ 
+        public function getPassword()
+        {
+                return $this->password;
+        }
+
+        /**
+         * Set the value of password
+         *
+         * @return  self
+         */ 
+        public function setPassword($password)
+        {
+                $this->password = $password;
+
+                return $this;
         }
     }
     
