bd1c9b51a292ea768ea158c82626e0a152521105
Isaya M Pombo <isayamlelapombo@gmail.com>
2020-07-30 10:11:53 +0200

    Utilisateur construct defined

--- utilisateur/Utilisateur.php
+++ utilisateur/Utilisateur.php
@@ -10,6 +10,14 @@
         private $prenom;
         private $mail;
         private $password;
+
+        public function __construct($nom, $prenom, $mail, $password)
+        {
+            $this->nom = $nom;
+            $this->prenom = $prenom;
+            $this->mail = $mail;
+            $this->password = $password;
+        }
     }
     
 
